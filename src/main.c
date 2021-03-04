cc931a61138a6545f0ea38818efa675ddc5e5637
rkyouma <r.kyouma@yandex.com>
2021-03-04 21:13:51 +0300

    write constant to the constant pool

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@ int main(int argc, char** argv) {
     Chunk chunk;
     init_chunk(&chunk);
 
-    write_chunk(&chunk, OP_RETURN, 0);
-    disassemble_chunk(&chunk, "test chunk");
+    write_chunk(&chunk, OP_CONSTANT, 0);
+    write_chunk(&chunk, add_constant(&chunk, 1.2), 0);
+    write_chunk(&chunk, OP_RETURN, 1);
 
+    disassemble_chunk(&chunk, "test chunk");
     free_chunk(&chunk);
     return 0;
 }
